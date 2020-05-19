bc3735d97382d12e5e6cc89671886999a68600c3
Sonja Parsell <hello@sparsell.com>
2020-05-19 09:56:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,8 +12,8 @@ class Scraper
     doc.css(".post").each do |post|
       course = Course.new
       course.title = post.css("h2").text
-      course.description = post.css(".date").text
-      course.title = post.css("p").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
     binding.pry
   end
